b699a3d3c78fc8a885506caf775f67fe9f11e187
MuXiu1997 <muxiu1997@gmail.com>
2021-04-15 16:16:42 +0800

    iterators2.rs

--- standard_library_types/iterators2.rs
+++ standard_library_types/iterators2.rs
@@ -13,7 +13,7 @@ pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => first.collect::<String>() + c.as_str(),
+        Some(first) => first.to_uppercase().collect::<String>() + c.as_str(),
     }
 }
 
@@ -37,14 +37,17 @@ mod tests {
     #[test]
     fn test_iterate_string_vec() {
         let words = vec!["hello", "world"];
-        let capitalized_words: Vec<String> = // TODO
+        let capitalized_words: Vec<String> = words.iter().map(|x| capitalize_first(*x)).collect();
         assert_eq!(capitalized_words, ["Hello", "World"]);
     }
 
     #[test]
     fn test_iterate_into_string() {
         let words = vec!["hello", " ", "world"];
-        let capitalized_words = // TODO
+        let capitalized_words = words
+            .iter()
+            .map(|x| capitalize_first(*x))
+            .collect::<String>();
         assert_eq!(capitalized_words, "Hello World");
     }
 }
